7cc437b3ea1506bd0f690de76a0fc574fc4d8bfc
gllherme <guicampos159@gmail.com>
2022-11-04 18:41:26 -0300

    [#] Inicial

---
+++ functions.py
@@ -0,0 +1,17 @@
+from formulas import *
+
+def subformulas(formula):
+    if isinstance(formula, Atom):
+        return {formula}
+    if isinstance(formula, Not):
+        return {formula}.union(subformulas(formula.inner))
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        sub1 = subformulas(formula.left)
+        sub2 = subformulas(formula.right)
+        return {formula}.union(sub1).union(sub2)
+
+#  we have shown in class that, for all formula A, len(subformulas(A)) <= length(A).
+
+# def atoms(formula):
+#     if isinstance
+    
---
+++ main.py
@@ -0,0 +1,25 @@
+from formulas import *
+from functions import *
+
+formula1 = Atom('p')  # p
+formula2 = Atom('q')  # q
+formula3 = And(formula1, formula2)  # (p /\ q)
+formula4 = And(Atom('p'), Atom('s'))  # (p /\ s)
+formula5 = Not(And(Atom('p'), Atom('s')))  # (¬(p /\ s))
+formula6 = Or(Not(And(Atom('p'), Atom('s'))), Atom('q'))  # ((¬(p /\ s)) v q)
+formula7 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Atom('r')))  # ((¬(p /\ s)) -> (q /\ r))
+formula8 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Not(And(Atom('p'), Atom('s'))))) # ((¬(p /\ s)) -> (q /\ (¬(p /\ s))))
+
+print('formula1:', formula1)
+print('formula2:', formula2)
+print('formula3:', formula3)
+print('formula4:', formula4)
+print('formula5:', formula5)
+print('formula6:', formula6)
+print('formula7:', formula7)
+print('formula8:', formula8)
+
+print('subformulas of formula7:')
+print(subformulas(formula7))
+for subformula in subformulas(formula7):
+    print(subformula)
